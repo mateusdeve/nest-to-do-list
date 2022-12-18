c2d57627e470b04e57691b89fd8aadb29fbd25b2
Mateus Pires <mateus.dev.ti@gmail.com>
2022-12-17 23:49:36 -0300

    adicionando algumas dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todos/entities/todo.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysqlsrv',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [Todo],
+      synchronize: true,
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
